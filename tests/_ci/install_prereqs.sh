8d819d067b2b045aa5979c4e3af85ef940dcd161
Hristomir Kotzev <izopi4a@gmail.com>
2018-11-13 13:40:21 +0200

    t2

--- tests/_ci/install_prereqs.sh
+++ tests/_ci/install_prereqs.sh
@@ -1,3 +1,38 @@
 #!/usr/bin/env bash
 
+set -e
 
+pushd /tmp
+
+# |--------------------------------------------------------------------------
+# | zephir parser
+# |--------------------------------------------------------------------------
+# |
+# | installs latest zephir parser extension
+# |
+
+git clone --depth=1 -v https://github.com/phalcon/php-zephir-parser.git -b ${ZEPHIR_PARSER_VERSION}
+cd php-zephir-parser
+phpize
+./configure
+make
+make install
+
+popd
+
+echo "extension=zephir_parser.so" >> $(phpenv root)/versions/$(phpenv version-name)/etc/conf.d/zephir-parser.ini
+
+php --ri 'Zephir Parser'
+
+# |--------------------------------------------------------------------------
+# | zephir
+# |--------------------------------------------------------------------------
+# |
+# | installs latest zephir extension
+# | DO NOT DELETE THE DIRECTORY
+# |
+
+mkdir -p /usr/share/zephir
+git clone --depth=1 -v https://github.com/phalcon/zephir /tmp/zephir
+cd /tmp/zephir
+./install -c && composer install --ignore-platform-reqs
